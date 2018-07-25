d30ace6a8b4f77fa12a8d84e6100e26d923133a3
Steve <sbrennison@gmail.com>
2018-07-25 09:58:37 -0500

    fixed styling error

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -18,19 +18,20 @@ router.use((request, response, next) => {
 
 
 
-
+// User menu page
 router.get('/', async (req, res, next)=>{
     try {
-
      const foundAllItem = await Item.find(req.body);
         res.render('items/index.ejs', {
-          items: foundAllItem
+          items: foundAllItem,
+          username: req.session.username,
+          loggedIn: req.session.loggedIn
       });
     } catch (err) {
-
       next(err)
     }
 });
+
 
 //-------------------------------------------------------
 // admin *New* items menu

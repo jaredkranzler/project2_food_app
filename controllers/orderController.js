9825c843745abd778bc287962ce5eee0dcb795e4
Jared Kranzler <kranzler312@gmail.com>
2018-07-25 13:17:56 -0500

    pseudcode for refactoring of cart

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -10,11 +10,27 @@ const User = require('../models/user')
 
 
 
-// User menu page
+// User menu page (index) show a list of items, add to cart and begin new order
+// URL should be get /:id
+// load current order
+// show list of items for user to add
+
+// show what is currently in their cart
+// ORDER SHOW ROUTE
+// should render orders/show.ejs
+// change orders/index.ejs to be a list of past orders
+
+// this route is like the cart
+// optionally, you could conditionally show a create order button here if there isn't one
+// note: this is also kind of an edit route 
+// because user can change cart
 router.get('/', async (req, res, next)=>{
     try {
+      const foundOrder = await Order.find({});
       const foundAllItem = await Item.find({});
+        // this should be orders/show.ejs,
         res.render('orders/index.ejs', {
+          orders: foundOrder, 
           items: foundAllItem,
           username: req.session.username,
           loggedIn: req.session.loggedIn
@@ -22,30 +38,38 @@ router.get('/', async (req, res, next)=>{
     } catch (err) {
       next(err)
     }
+
+
 });
 
 
-// create route -- add to data
-router.post('/', async (req, res, next) => {
-    try {
-        const createdItem = await Order.create(req.body);
-        res.redirect('/orders/new')
-    } catch (err) {
-      next(err, "hey")     
-    }
-});
+// another place create order button could go 
+// is in order index.  create order index route here
+// order INDEX should:
+  // always show previous orders
+  // AND if there isn't an open order show a button to create one
+  // ORDER index route -- 
+// URL SHOULD BE get /
 
 
 
-// Orders/CART.ejs (index)
-// // CART: (order show page) 
-router.get('/new', async (req, res) => {
+
+// // new: (new order) 
+// goal: list the items in the current order
+// IGNORE this for now
+router.get('/new', async (req, res, next) => {
+  console.log("")
+  console.log("")
+  console.log("")
+  console.log("hitting /orders/new")
+  console.log("")
+  console.log("")
+  console.log("")
   try{
-    // const foundAllItems = await Item.find({});
-    const foundOrder = await Order.find({});
+    const foundOrders = await Order.find({});
     res.render('orders/new.ejs', {
-      // items: foundAllItems,
-      orders: foundOrder, 
+      orders: foundOrders,
+      items: foundOrders,
       username: req.session.username,
       loggedIn: req.session.loggedIn
     });
@@ -55,11 +79,51 @@ router.get('/new', async (req, res) => {
   }
 });
 
-// create route -- add to data
+
+
+
+// ORDER CREATE route -- the button mentioned above should hit this route
+// this route should:
+//   create an order
+//   store info in session to indicate that
+//   there's an open order
+router.post('/', async (req, res, next) => {
+    try {
+
+        const createdOrder = await Order.create(req.body);
+
+        res.redirect('/orders', {     
+          username: req.session.username,
+          loggedIn: req.session.loggedIn
+        })
+
+    }  catch (err){
+
+      next(err, "hey")
+      
+    }
+});
+
+// add to order
+// change url to additem
+// post /additem --> POST /orders/additem
+
+  // get current order object from database
+  // get item from database
+  // push into items array of current order object you just got from db
+  // redirect to order show page (so user can see item got added)
+
 router.post('/new', async (req, res, next) => {
     try {
-        const createdItem = await Order.create(req.body);
-        res.redirect('/orders')
+        // get item with this id from items collection
+        // push into items array in the currently open order
+        // which should be an Order (model) that was previously creatd
+
+        // get all the items in this variable
+        const foundItem  = await Item.find(req.body);
+
+        // redirect
+        res.redirect('/new')
 
     }  catch (err){
 
@@ -70,8 +134,21 @@ router.post('/new', async (req, res, next) => {
 
 
 
+
 //--------------------------------------------------------------------------------------
 // PUT (UPDATE)
+
+
+/// should be 1 or more routes hit by buttons 
+/// (like delete, +, -) on the show page
+// the route(s) should adjust order in db as necessary
+// and then redirect to show page
+
+// URLS/routes you might need
+  // put /remove/:orderid/:itemid/
+  // put /increase/:orderid/:itemid/
+  // put /decrease/:orderid/:itemid/
+
 
 router.put('/:id', async (req, res) => {
   try {
@@ -92,19 +169,19 @@ router.put('/:id', async (req, res) => {
 //--------------------------------------------------------------------------------------
 // DELETE
 
-router.delete('/:id', async (req, res) => {
+// cancel the order
+// this route should be hit by a "Cancel order button"
+// this may mean you need a cancel order button on your show page
+router.delete('/new', async (req, res, next) => {
   try {
-    const deletedOrder = await Order.findByIdAndRemove(req.params.id)
-    const itemIds = [];
-    for (let i = 0; i < deletedOrder.items.length; i++) {
-      itemIds.push(deletedOrder.items[i].id);
-    }
-      const data = await Item.remove({_id: { $in: itemIds}});
-      res.redirect('/orders');
+    const foundItem   = await Item.findOne(req.body).remove();
+    res.redirect('./new')
   } catch (err) {
-    res.send(err, 'delete route messed up')
+    next(err, '<------------delete')
+    res.send(err)
   }
 });
+
 
 //--------------------------------------------------------------------------------------
 module.exports = router;

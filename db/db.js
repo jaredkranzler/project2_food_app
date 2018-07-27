59c20a825476ac05bacd3ce371f4d11760797de6
Steve <sbrennison@gmail.com>
2018-07-27 10:33:38 -0500

    resolved merge conflicts

--- db/db.js
+++ db/db.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose');
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/restauranteur';
 
-<<<<<<< HEAD
-mongoose.connect(mongoUri)
-=======
-const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/restauranteur';
-
 mongoose.connect('mongoUri')
->>>>>>> 0477903fb0122c03139a624b4549a534af8aa9cd
 
 mongoose.connection.on('connected', () => {
   console.log("mongoose is connected")
